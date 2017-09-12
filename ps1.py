9798bbd1e731bff1a0f6bc1b7a848a57764b814d
jtm172186 <sujeetmandloi23@gmail.com>
2017-09-12 15:59:39 +0530

    Update 1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,35 @@
 ###### this is the first .py file ###########
 
 ####### write your code here ##########
+
+s=raw_input('')    #input from user
+
+i=s.count('1')    # counts the no of 1's 
+
+
+if i%2==0 :       # check whether even no of ones or odd if even then add 1 
+	s=s+"1"
+	print(s)
+	k=s.replace('010','0100',s.count('010')) # insert 0 when 010 is found
+	
+	if s[-2:]=="01":   #if last two bit are 01 then replace with 010 and and insert flag also
+		k=k[:-2]+"0100101"
+		print(k)
+	else : 
+		k=k+"0101"
+		print(k)
+	
+
+ 	
+
+
+
+else  :
+	print(s)
+	s.replace('010','0100',s.count('010'))
+	if s[-2:]=="01":
+		k=s[:-2]+"0100101"
+		print(k)
+	else : 
+		k=k+"0101"
+		print(k)
